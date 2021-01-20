9129699ee13139f1be6e34f81521fa48d0754b86
wayne <juchuang123@gmail.com>
2021-01-20 21:52:42 +0900

    ModelSpecの追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "メール、パスワードがある場合有効" do
+    user = FactoryBot.build(:user)
+    expect(user).to be_valid
+  end
+
+  it "メールアドレスがない場合、無効" do
+    user = FactoryBot.build(:user, email: nil)
+    expect(user).to_not be_valid
+  end
+  it "重複したメールアドレスの場合、無効" do
+    user = FactoryBot.create(:user, email: "duple@example.com")
+    user2 = FactoryBot.build(:user, email: user.email)
+    expect(user2).to_not be_valid
+  end
+
+  it "パスワードがない場合、無効" do
+    user = FactoryBot.build(:user, password: "")
+    expect(user).to_not be_valid
+  end
 end
