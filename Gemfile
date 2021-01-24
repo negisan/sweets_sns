0585dc22964d28416bf6d1f1a26bc0bea08d351c
wayne <juchuang123@gmail.com>
2021-01-24 13:33:49 +0900

    tailwindcssの設定をtailwindcss-railsのGemを利用した設定に変更

--- Gemfile
+++ Gemfile
@@ -64,3 +64,4 @@ gem 'devise-i18n'
 gem 'devise-i18n-views'
 
 gem 'pg'
+gem "tailwindcss-rails", "~> 0.3.1"
--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,6 @@ module.exports = {
   plugins: [
     require('postcss-import'),
     require('postcss-flexbugs-fixes'),
-    require('tailwindcss'),
-    require('autoprefixer'),
     require('postcss-preset-env')({
       autoprefixer: {
         flexbox: 'no-2009'
@@ -12,19 +10,3 @@ module.exports = {
     })
   ]
 }
-
-// 使っているクラスだけを抽出する設定
-if (process.env.RAILS_ENV === "production") {
-  environment.plugins.push(
-    require('@fullhuman/postcss-purgecss')({
-      content: [
-        './app/**/*.html.erb',
-        './app/**/*.js.erb',
-        './app/helpers/**/*.rb',
-      ],
-      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-    })
-  )
-}
-
-module.exports = environment
