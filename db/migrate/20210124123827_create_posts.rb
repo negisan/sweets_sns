1b903532d6b1c4aaa3314c1c5155ae9e30933c43
wayne <juchuang123@gmail.com>
2021-01-26 20:36:46 +0900

    migrationファイルを修正

--- db/migrate/20210124123827_create_posts.rb
+++ db/migrate/20210124123827_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[6.0]
   def change
     create_table :posts do |t|
-      t.string :post_image, :string
-      t.string :post_content, :string
+      t.string :post_image
+      t.string :post_content
 
       t.timestamps
     end
--- db/migrate/20210126110103_change_data_to_posts.rb
+++
@@ -1,6 +0,0 @@
-class ChangeDataToPosts < ActiveRecord::Migration[6.0]
-  def change
-    change_column :posts, :image, :string
-    change_column :posts, :body, :string
-  end
-end
