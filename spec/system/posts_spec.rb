ef67c29d27fed650e2e2b5f25292f866f1219495
wayne <juchuang123@gmail.com>
2021-01-27 21:04:50 +0900

    pending状態のspecを記述

--- spec/system/posts_spec.rb
+++ spec/system/posts_spec.rb
@@ -2,29 +2,47 @@ require 'rails_helper'
 
 RSpec.describe 'ログインしていないユーザー', type: :system do
   describe 'テンプレートの表示' do
+    let(:user) { FactoryBot.create(:user) }
+    let!(:post) { FactoryBot.create(:post, user_id: user.id) }
+
     describe 'posts/show' do
-      it "showページにアクセスできる"
+      it "showページにアクセスできる" do
+        visit post_path(post)
+        expect(page).to have_selector 'img[alt="sample.jpg"]'
+      end
 
-      it '編集リンクが表示されない'
+      it '編集リンクが表示されない' do
+        visit post_path(post)
+        expect(page).to_not have_selector 'a', text: '編集'
+      end
 
-      it '削除リンクが表示されない'
+      it '削除リンクが表示されない' do
+        visit post_path(post)
+        expect(page).to_not have_selector 'a', text: '削除'
+      end
     end
 
     describe 'posts/new' do
-      it "newページへアクセスできない"
+      it "newページへアクセスできない" do
+        visit post_path(post)
+        expect(page).to_not have_content '投稿の新規作成'
+      end
     end
   end
 end
 
 
+
 RSpec.describe 'ログインしているユーザー', type: :system do
   describe 'テンプレートの表示' do
     let!(:user) { FactoryBot.create(:user) }
+    let!(:user2) { FactoryBot.create(:user) }
     let!(:post) { FactoryBot.create(:post, user_id: user.id) }
 
     before do
       sign_in user
     end
+
 
     describe "posts/new" do
       it "newページへアクセスできる" do
@@ -33,21 +51,48 @@ RSpec.describe 'ログインしているユーザー', type: :system do
       end
     end
 
+
     describe "posts/show" do
       it 'showページにアクセスできる' do
         visit post_path(post)
         expect(page).to have_content '投稿詳細'
       end
 
-      it "画像が表示される"
+      it "画像が表示される" do
+        visit post_path(post)
+        expect(page).to have_selector 'img[alt="sample.jpg"]'
+      end
 
-      it 'postを作成したユーザーと現在のユーザーが一致したら、編集リンクが表示される'
 
-      it 'postを作成したユーザーと現在のユーザーが一致したら、削除リンクが表示される'
+      context 'postを作成したユーザーとログインしているユーザーが一致' do
+        before do
+          visit post_path(post)
+        end
 
-      it 'postを作成したユーザーと現在のユーザーが一致しない場合、編集リンクが表示されない'
+        it '編集リンクが表示される' do
+          expect(page).to have_selector 'a', text: '編集'
+        end
 
-      it 'postを作成したユーザーと現在のユーザーが一致しない場合、削除リンクが表示されない'
+        it '削除リンクが表示される' do
+          expect(page).to have_selector 'a', text: '削除'
+        end
+      end
+
+
+      context 'postを作成したユーザーとログインしているユーザーが違う' do
+        before do
+          sign_in user2
+          visit post_path(post)
+        end
+
+        it '編集リンクが表示されない' do
+          expect(page).to_not have_selector 'a', text: '編集'
+        end
+
+        it '削除リンクが表示されない' do
+          expect(page).to_not have_selector 'a', text: '削除'
+        end
+      end
     end
   end
 
