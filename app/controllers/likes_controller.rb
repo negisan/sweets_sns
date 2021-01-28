c1a8262dec3cbf24f5c42475094593b909fad627
wayne <juchuang123@gmail.com>
2021-01-28 17:02:50 +0900

    likeボタンが反映されないバグを修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,11 @@
 class LikesController < ApplicationController
   def create
-    @post = Post.find(params[:post_id])
-    like = current_user.likes.build(post_id: params[:post_id])
-    like.save
-
+    @post = Post.find_by(id: params[:post_id])
+    like = current_user.likes.create(post_id: params[:post_id])
   end
 
   def destroy
-    @post = Post.find(params[:post_id])
+    @post = Post.find_by(id: params[:post_id])
     like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
     like.destroy
   end
