dcce0faa28bcd5f20eb2ab7e197e22510dacbe1b
wayne <juchuang123@gmail.com>
2021-01-28 14:44:52 +0900

    likeボタンをajaxで実装

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,11 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
+require('jquery')
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require('jquery')
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -19,6 +19,7 @@ require('jquery')
 
 
 //= require jquery3
+//= require rails-ujs
 //= require popper
 //= require bootstrap-sprockets
 
