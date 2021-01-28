4256b65b48a7d71465b80b62714395b68aaee391
wayne <juchuang123@gmail.com>
2021-01-28 10:57:45 +0900

    likeモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
+  has_many :liked_posts, through: :likes, source: :post
 end
