be54c302d94e22be36f9af6509c6d6fba09e6051
wayne <juchuang123@gmail.com>
2021-01-31 16:41:00 +0900

    ユーザーページにページ機能を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @posts = @user.posts
+    @posts = @user.posts.page(params[:page]).per(8)
   end
 end
