7bee307de8b7c88468858a8069db7e1dcd4554c5
wayne <juchuang123@gmail.com>
2021-02-04 14:55:33 +0900

    ローソンの新着商品をスクレイピングして表示

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,6 @@
 class HomeController < ApplicationController
+  require 'mechanize'
+
   def index
     @posts = Post.all.order(created_at: :desc).limit(4)
 
@@ -10,5 +12,10 @@ class HomeController < ApplicationController
 
     posts_likes_all_time_desc = Post.includes(:liked_users).sort {|a,b| b.liked_users.size <=> a.liked_users.size}
     @posts_likes_all_time = posts_likes_all_time_desc.first(4)
+
+
+    agent = Mechanize.new
+    page = agent.get("https://www.lawson.co.jp/recommend/original/dessert/")
+    @elements = page.search('article#dessert li a').first(4)
   end
 end
