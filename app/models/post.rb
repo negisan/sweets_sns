0706e17f0a267807063be7c5a9866ed40586dea4
wayne <juchuang123@gmail.com>
2021-02-05 17:30:08 +0900

    コメントモデルのアソシエーションの記述

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :likes
   has_many :liked_users, :through => :likes, :source => :user
+  has_many :comments
 
   validates :image, presence: true, blob: {content_type: :image}
   validates :body, length: {maximum: 200}
