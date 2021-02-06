df963881e0c393d0350c131dcde08043187180c0
wayne <juchuang123@gmail.com>
2021-02-06 11:55:31 +0900

    モデルの依存関係を設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
   has_many :liked_posts, :through => :likes, :source => :post
-  has_many :comments
-
+  has_many :comments, dependent: :destroy
   def already_liked?(post)
     self.likes.exists?(post_id: post.id)
   end
